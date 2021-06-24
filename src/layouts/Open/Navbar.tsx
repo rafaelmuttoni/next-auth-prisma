6b1731331753c90e72292ead08ec2f9abbbbd399
Rafael Muttoni <hi@rafaelmuttoni.com>
2021-06-23 23:57:20 -0300

    small stuff

--- src/layouts/Open/Navbar.tsx
+++ src/layouts/Open/Navbar.tsx
@@ -32,7 +32,7 @@ export default function WithSubnavigation() {
         color={useColorModeValue("gray.600", "white")}
         minH={"60px"}
         py={{ base: 2 }}
-        px={{ base: 4 }}
+        px={{ base: 6 }}
         borderBottom={1}
         borderStyle={"solid"}
         borderColor={useColorModeValue("gray.200", "gray.900")}
--- src/layouts/Open/index.tsx
+++ src/layouts/Open/index.tsx
@@ -1,10 +1,11 @@
+import { Box } from "@chakra-ui/react";
 import Navbar from "./Navbar";
 
 export default function OpenLayout({ children }) {
   return (
-    <>
+    <Box>
       <Navbar />
       {children}
-    </>
+    </Box>
   );
 }
